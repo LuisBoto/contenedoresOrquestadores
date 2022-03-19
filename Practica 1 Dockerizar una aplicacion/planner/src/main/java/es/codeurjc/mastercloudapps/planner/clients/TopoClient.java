f0fd9ef4f33d01a21a35d980b9b1c66b83c0c5c6
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-03-19 14:04:36 +0100

    RabbitMQ working on planner

--- Practica 1 Dockerizar una aplicacion/planner/src/main/java/es/codeurjc/mastercloudapps/planner/clients/TopoClient.java
+++ Practica 1 Dockerizar una aplicacion/planner/src/main/java/es/codeurjc/mastercloudapps/planner/clients/TopoClient.java
@@ -1,6 +1,8 @@
 package es.codeurjc.mastercloudapps.planner.clients;
 
 import es.codeurjc.mastercloudapps.planner.models.LandscapeResponse;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,15 +12,15 @@ import java.util.concurrent.CompletableFuture;
 @Service
 public class TopoClient {
 
-    private static final String TOPO_HOST = "localhost";
-    private static final int TOPO_PORT = 8181;
+	@Value("${topo.client.address}")
+    private String TOPO_ADDRESS; // = "localhost:8181";
 
     @Async
     public CompletableFuture<String> getLandscape(String city) {
         
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "http://"+TOPO_HOST+":"+TOPO_PORT+"/api/topographicdetails/" + city;
+        String url = "http://"+TOPO_ADDRESS+"/api/topographicdetails/" + city;
         
         LandscapeResponse response = restTemplate.getForObject(url, LandscapeResponse.class);
         
