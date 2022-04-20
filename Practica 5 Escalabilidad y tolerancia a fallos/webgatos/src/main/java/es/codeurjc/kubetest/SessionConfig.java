527bece4995c986016b257c0e26525e0a87f4b41
luisboto <43352462+LuisBoto@users.noreply.github.com>
2022-04-20 21:37:51 +0200

    Fixed session config

--- Practica 5 Escalabilidad y tolerancia a fallos/webgatos/src/main/java/es/codeurjc/kubetest/SessionConfig.java
+++ Practica 5 Escalabilidad y tolerancia a fallos/webgatos/src/main/java/es/codeurjc/kubetest/SessionConfig.java
@@ -2,13 +2,15 @@ package es.codeurjc.kubetest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.hazelcast.HazelcastIndexedSessionRepository;
-import org.springframework.session.hazelcast.PrincipalNameExtractor;
+import org.springframework.session.hazelcast.Hazelcast4IndexedSessionRepository;
+import org.springframework.session.hazelcast.Hazelcast4PrincipalNameExtractor;
+import org.springframework.session.hazelcast.config.annotation.SpringSessionHazelcastInstance;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 import com.hazelcast.config.AttributeConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.IndexConfig;
+import com.hazelcast.config.IndexType;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
@@ -17,18 +19,20 @@ import com.hazelcast.core.HazelcastInstance;
 public class SessionConfig {
 
 	@Bean
+	@SpringSessionHazelcastInstance
 	public HazelcastInstance hazelcastInstance() {
 		AttributeConfig attributeConfig = new AttributeConfig()
-				.setName(HazelcastIndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
-				.setExtractorClassName(PrincipalNameExtractor.class.getName());
+				.setName(Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+				.setExtractorClassName(Hazelcast4PrincipalNameExtractor.class.getName());
 
 		Config config = new Config();
 
-		config.getMapConfig(HazelcastIndexedSessionRepository.DEFAULT_SESSION_MAP_NAME)
+		config.getMapConfig(Hazelcast4IndexedSessionRepository.DEFAULT_SESSION_MAP_NAME)
 				.addAttributeConfig(attributeConfig)
 				.addIndexConfig(
 						new IndexConfig()
-						.setName(HazelcastIndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE));
+						.addAttribute(Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+						.setType(IndexType.HASH));
 
 		return Hazelcast.newHazelcastInstance(config);
 	}
